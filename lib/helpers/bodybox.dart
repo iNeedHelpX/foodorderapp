a562470f02414540d253ca793da7684d497d65fb
iNeedHelpX <jpak0444@gmail.com>
2021-01-24 10:36:35 -0500

    changes to size

--- lib/helpers/bodybox.dart
+++ lib/helpers/bodybox.dart
@@ -17,7 +17,7 @@ class Box extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           // SizedBox(height: 60),
-          SizedBox(height: 190),
+          SizedBox(height: 180),
           Stack(
             alignment: Alignment.topCenter,
             clipBehavior: Clip.antiAliasWithSaveLayer,
